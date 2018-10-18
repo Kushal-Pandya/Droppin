bcf8b56d55c501da89c9f1bc5aeb06b69cc2778b
Adnan Ahmed <adnan_a96@hotmail.com>
2018-10-18 12:09:37 -0400

    added fb login button

--- Droppin/LoginViewController.swift
+++ Droppin/LoginViewController.swift
@@ -7,29 +7,62 @@
 //
 
 import UIKit
+import FirebaseUI
+import FBSDKLoginKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     @IBOutlet weak var login_Email: UITextField!
     @IBOutlet weak var login_Password: UITextField!
+
+    @IBOutlet weak var fbLoginBtn: FBSDKLoginButton!
+    
+    
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        loginButton.frame.size = CGSize(width: fbLoginBtn.frame.width, height: 100)
+        loginButton.delegate = self
+        fbLoginBtn = loginButton
+        
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
     }
     
-
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func fetchProfile() {
+        print("fetch profile")
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start(completionHandler: { (connection, result, error) -> Void in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            if let email = result as? [String:Any] {
+                print(email["email"]!)
+            }
+        })
     }
-    */
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Logged out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("Successfully logged in to facebook...")
+        fetchProfile()
+    }
 
 }
