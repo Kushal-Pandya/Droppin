e0a28a9d3ed3ba640a3e2c3c9dd6c08f75824dc3
Syed Ahmed <syedahmed@Shahrukhs-MacBook-Pro.local>
2018-11-13 18:18:44 -0500

    Events page segmented control added

--- Droppin/EventDetailsViewController.swift
+++ Droppin/EventDetailsViewController.swift
@@ -8,6 +8,105 @@
 
 import UIKit
 
+@IBDesignable
+class DesignableView: UIView {
+}
+
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+extension UIView {
+    
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
 class EventDetailsViewController: UIViewController {
     var textTitle:String = ""
     var textDescription:String = ""
@@ -23,6 +122,9 @@ class EventDetailsViewController: UIViewController {
     
     
     
+    @IBAction func cancelButton(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
+    }
     @IBAction func goButton(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
@@ -31,7 +133,20 @@ class EventDetailsViewController: UIViewController {
         
         eventTitle?.text = textTitle
         eventDescription?.text = textDescription
-        eventDate?.text = textDate
+        // MMM d, EEEE, h:mm a
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss +zzzz"
+        
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "EEEE, MMM d, h:mm a"
+        
+        if let date = dateFormatterGet.date(from: textDate) {
+            eventDate?.text = dateFormatterPrint.string(from: date)
+            print(dateFormatterPrint.string(from: date))
+        } else {
+            print("There was an error decoding the string")
+        }
+        
         eventCategory?.text = textCategory
         eventLocation?.text = textLocation
         // Do any additional setup after loading the view.
