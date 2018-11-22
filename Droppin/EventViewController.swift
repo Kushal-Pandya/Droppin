5102a77c964cf42d6f4f5129afe4d5f78ce066be
Kushal Pandya <kushalpandya@Kushals-MacBook-Pro.local>
2018-11-22 17:18:31 -0500

    added table view options for events

--- Droppin/EventViewController.swift
+++ Droppin/EventViewController.swift
@@ -44,6 +44,82 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
         
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Add event details page here, will need to make API call
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            let edit = UITableViewRowAction(style: .normal, title: "Edit") { action, index in
+                
+            }
+            
+            let delete = UITableViewRowAction(style: .destructive, title: "Delete") { action, index in
+                let cell = tableView.cellForRow(at: index)
+                let cellTitle = cell?.textLabel?.text
+                
+                let alert = UIAlertController(
+                    title: "Are you sure you want to delete " + cellTitle! + " ?",
+                    message: "This will permanently delete the event and notify all attendees.",
+                    preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(
+                    title: "Delete",
+                    style: .destructive,
+                    handler: { action in
+                        // Delete API call here
+                }))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            return [delete, edit]
+        case 1:
+            let accept = UITableViewRowAction(style: .normal, title: "Accept") { action, index in
+                let cell = tableView.cellForRow(at: index)
+                let cellTitle = cell?.textLabel?.text
+                
+                let alert = UIAlertController(
+                    title: "Accepted Event!",
+                    message: "Congrats you are going to " + cellTitle!,
+                    preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            accept.backgroundColor = .blue
+            
+            let tentative = UITableViewRowAction(style: .normal, title: "Tentative") { action, index in
+                let cell = tableView.cellForRow(at: index)
+                let cellTitle = cell?.textLabel?.text
+                
+                let alert = UIAlertController(
+                    title: "Tentative Event",
+                    message: "You might be going to " + cellTitle!,
+                    preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            let decline = UITableViewRowAction(style: .destructive, title: "Decline") { action, index in
+                let cell = tableView.cellForRow(at: index)
+                let cellTitle = cell?.textLabel?.text
+                
+                let alert = UIAlertController(
+                    title: "Declined Event",
+                    message: "You have declined your invitation to " + cellTitle!,
+                    preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+            return [decline, tentative, accept]
+        default:
+            break
+        }
+        return []
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
